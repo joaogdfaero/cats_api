d3284cb60acbcda175878d714d43483400eb126b
João Gabriel Dal Forno <joao.dal@acad.ufsm.br>
2023-09-07 09:21:48 -0300

    using API data in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+# COLLECTING API DATA
 response = RestClient.get(ENV["BASE_URL"]+"/breeds")
 breeds_array = JSON.parse(response)
 
+#USING API DATA COLLECTED
+breeds_array.each do |breed|
+    Cat.create(name: breed["name"], origin: breed["origin"])
+end
 
-binding.pry
